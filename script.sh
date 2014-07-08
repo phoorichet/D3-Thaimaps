f51d95eba25df34f06971b380085b56923ae07f7
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2014-07-08 15:25:21 +0700

    Added function call for the script

--- script.sh
+++ script.sh
@@ -17,22 +17,68 @@ gen_places()
 }
 
 
+gen_maps()
+{
+  echo "$1, $2"
+  if [ -e $2 ]
+  then
+    echo "Removing $2"
+    rm $2
+  fi
+
+  tmp_file="maps/tmp_file.json"
+  if [ -e $tmp_file ]
+  then
+    rm $tmp_file
+  fi
+
+  echo "Generating GeoJSON from shapefile.. $1"
+  ogr2ogr -f GeoJSON $tmp_file $1
+  echo "Generating TopoJSON from GeoJSON file.. $tmp_file"
+  topojson -o $2 --id-property ID_0 --properties -- $tmp_file $FILE_PLACES
+  echo "output.. $2"
+  echo 'Done..!\n'
+
+  if [ -e $tmp_file ]
+  then
+    rm $tmp_file
+  fi
+
+}
+
 
 ## MAIN ###
 gen_places
 
-FILE1='maps/tha_adm1.json'
-FILE2='maps/subunit_adm1.json'
-if [ -e $FILE1 ] || [ -e $FILE2 ];
-then
-  # Remove the existing files
-  echo "Removing files $FILE1 $FILE2"
-  rm $FILE1
-  rm $FILE2
-fi
 
-echo 'Generating GeoJSON from shapefile'
-ogr2ogr -f GeoJSON maps/subunit_adm1.json THA_adm/THA_adm1.shp
-echo 'Generating TopoJSON from GeoJSON file'
-topojson -o maps/tha_adm1.json --id-property ID_0 --properties -- maps/subunit_adm1.json $FILE_PLACES
-echo 'Done..!'
+FILE1="THA_adm/THA_adm0.shp"
+FILE2="maps/tha_adm0.json"
+gen_maps $FILE1 $FILE2
+
+FILE1="THA_adm/THA_adm1.shp"
+FILE2="maps/tha_adm1.json"
+gen_maps $FILE1 $FILE2
+
+FILE1="THA_adm/THA_adm2.shp"
+FILE2="maps/tha_adm2.json"
+gen_maps $FILE1 $FILE2
+
+FILE1="THA_adm/THA_adm3.shp"
+FILE2="maps/tha_adm3.json"
+gen_maps $FILE1 $FILE2
+
+# FILE1='maps/tha_adm1.json'
+# FILE2='maps/subunit_adm1.json'
+# if [ -e $FILE1 ] || [ -e $FILE2 ];
+# then
+#   # Remove the existing files
+#   echo "Removing files $FILE1 $FILE2"
+#   rm $FILE1
+#   rm $FILE2
+# fi
+# 
+# echo 'Generating GeoJSON from shapefile'
+# ogr2ogr -f GeoJSON maps/subunit_adm1.json THA_adm/THA_adm1.shp
+# echo 'Generating TopoJSON from GeoJSON file'
+# topojson -o maps/tha_adm1.json --id-property ID_0 --properties -- maps/subunit_adm1.json $FILE_PLACES
+# echo 'Done..!'
